5c6b600171ffd761d968d19ad1be26c9a0ef3a04
DanielDutraa <dutraniel013@gmail.com>
2023-10-12 20:22:24 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_Clubes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_Clubes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
