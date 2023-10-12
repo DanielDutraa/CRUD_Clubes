a399b5c8b8d22991efb3c4dc6756dd93df6be0c1
DanielDutraa <dutraniel013@gmail.com>
2023-10-12 20:38:19 -0300

    add classes

---
+++ CRUD_Clubes/Models/Clube.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUD_Clubes.Models
+{
+    public class Clube
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Série { get; set; }
+    }
+}
